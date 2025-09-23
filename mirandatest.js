d8d97c748feda0182eda0b64052ddfc456fb3546
tzzzzzzt <231790498+tzzzzzzt@users.noreply.github.com>
2025-09-23 09:45:11 -0300

    HICE Q EL CUBO SEA UNA IMAGEN Y SE PUEDA MOVERRR

--- mirandatest.js
+++ mirandatest.js
@@ -2,9 +2,10 @@
 var myGamePiece;
 
 function startGame() {
+    
     myGameArea.start();
-    myGamePiece = new component(50, 50, "red", 10, 120, "image");
-    myGameArea.update();
+    myGamePiece = new component(50, 50, "recursos/img/yotsucompresion.png", 10, 200,"image");
+    
 }
 
 var myGameArea = {
@@ -14,8 +15,17 @@ var myGameArea = {
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
-}
+ }
+
 
 function component(width, height, color, x, y, type) {
     this.type = type;
@@ -29,13 +39,24 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;    
     this.x = x;
     this.y = y;    
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function(){
+        ctx = myGameArea.context;
+        if (type == "image"){
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width, this.height
+            );
+        }else{
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;        
     }
+
 }
 
 function updateGameArea() {
@@ -44,11 +65,11 @@ function updateGameArea() {
     myGamePiece.update();
 }
 
-function bla(dir) {
-    myGamePiece.image.src = "recursos/img/yotsucompresion.png";
-    if (dir == "up") {myGamePiece.speedY = -111; }
-    if (dir == "down") {myGamePiece.speedY = 1; }
-    if (dir == "left") {myGamePiece.speedX = -1; }
-    if (dir == "right") {myGamePiece.speedX = 1; }
+function moverderecha() {
+    myGamePiece.speedX +=1
+    
+}
 
+function explotar() {
+    myGamePiece.image.src = "recursos/img/explosion.gif";
 }
