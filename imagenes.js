fe2e325af03eeecb32ed7580968ecfd3e7e36fff
tzzzzzzt <231790498+tzzzzzzt@users.noreply.github.com>
2025-10-02 13:23:32 -0300

    testestset

--- imagenes.js
+++ imagenes.js
@@ -18,11 +18,11 @@ function actualizarEstado() {
 
         var img = document.getElementById("petImg")
         const comer = new Audio("audio/omnomnom.mp3");
-        img.src = "img/heavyomnomnom.gif";
+        img.src = "img/mirandasketchomnomnom.gif";
         comer.play();
 
         setTimeout(function(){
-            img.src="img/heavyidle.jpeg";
+            img.src="img/mirandasketchidle.jpeg";
         }, 3000)
 
         actualizarEstado();
@@ -35,7 +35,7 @@ function actualizarEstado() {
         bailar.play();
 
         setTimeout(function(){
-            img.src="img/heavyidle.jpeg";
+            img.src="img/mirandasketchidle.jpeg";
         }, 3000)
     }
 
