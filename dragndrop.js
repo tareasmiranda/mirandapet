e966fc849cd4f6cd06d6e0fc02eacb73787c2906
tzzzzzzt <231790498+tzzzzzzt@users.noreply.github.com>
2025-10-02 11:52:13 -0300

    "puedes mover el bloke d yotsuba"

---
+++ dragndrop.js
@@ -0,0 +1,29 @@
+let newX = 0, newY = 0, startX = 0, startY = 0;
+
+const comida = document.getElementById('comida')
+
+comida.addEventListener('mousedown', mouseDown)
+
+function mouseDown(e){
+    startX = e.clientX
+    startY = e.clientY
+
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+    console.log("hoal")
+}
+
+function mouseMove(e){
+    newX = startX - e.clientX 
+    newY = startY - e.clientY 
+  
+    startX = e.clientX
+    startY = e.clientY
+
+    comida.style.top = (comida.offsetTop - newY) + 'px'
+    comida.style.left = (comida.offsetLeft - newX) + 'px'
+}
+
+function mouseUp(e){
+    document.removeEventListener('mousemove', mouseMove)
+}
